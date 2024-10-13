6ef02bcf93f468b1a3211a7bde46b9f17a8884b6
Andhikafdh <andhikafadhillah2211@gmail.com>
2024-10-13 17:05:02 +0700

    feat:add navbar

---
+++ php/src/views/navbar/navbarcomp.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../../../../assets/styles/navbar/navbarcomp.css">
+</head>
+    <body>
+        <div class="navbar">
+            <div class="navbar-left">
+                <i class='fab'>&#xf08c;</i>
+                <div class="search-bar">
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder="Title, skill or company">
+                </div>
+            </div>
+
+            <div class="navbar-right">
+                <div class="nav-item">
+                    <img class="change" src="../../../../assets/img/home.png" >
+                    <a href="#">Home</a>
+                </div>
+                <div class="nav-item">
+                    <img src="../../../../assets/img/photo.png">
+                    <a href="#">Company</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ php/src/views/navbar/navbarjs.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../../../../assets/styles/navbar/navbarjs.css">
+</head>
+<body>
+
+<div class="navbar">
+    <div class="navbar-left">
+        <i class='fab'>&#xf08c;</i>
+        <div class="search-bar">
+            <i class="fas fa-search"></i>
+            <input type="text" placeholder="Title, skill or company">
+        </div>
+    </div>
+
+    <div class="navbar-right">
+        <div class="nav-item">
+            <img class="change" src="../../../../assets/img/home.png">
+            <a href="#">Home</a>
+        </div>
+        <div class="nav-item">
+            <img class="change" src="../../../../assets/img/job.png">
+            <a href="#">My Job</a>  
+        </div>
+        <div class="nav-item">
+            <img src="../../../../assets/img/photo.png">
+            <a href="#">Me</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
