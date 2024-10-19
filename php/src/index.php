be151285f530a9bc92eff1f8dc8b85b16d9f220d
Rinnearu <rayhanridhar@gmail.com>
2024-10-19 15:33:19 +0700

    feat: Adding routing beginning

--- php/src/index.php
+++ php/src/index.php
@@ -1,62 +1,57 @@
 <?php
-spl_autoload_register(function ($class_name) {
-    require_once __DIR__ . '/models/' . $class_name . '.php';
-});
 
-session_start();
-require_once __DIR__ . '/controllers/LoginController.php';
+// var_dump($_SERVER);
+// var_dump($_POST);
 
-$page = isset($_GET['page']) ? $_GET['page'] : 'detail-lamaran';
+$uri_path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$path = explode('/', $uri_path);
 
-switch ($page) {
-    case 'login':
-        require_once __DIR__ . '/public/views/login/login.php';
-        break;
+$controller = !empty($path[0]) ? ucfirst($path[0]) . 'Controller' : 'HomeController';
+$action = !empty($path[1]) ? $path[1] : 'index';
+$params = array_slice($path,2);
 
-    case 'login-process':
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['password'])) {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $loginController = new LoginController();
-            $loginController->login($email, $password);
-        } else {
-            header("Location: index.php?page=login&error=Invalid request");
-            exit();
-        }
-        break;
+$controller_path = "controllers/" . $controller . ".php";
 
-    case 'register':
-        # code...
-        break;
+if (file_exists($controller_path)) {
+    require_once $controller_path;
 
-    case 'home':
-        require_once __DIR__ . '/public/views/homepage/homecomp.php';
-        break;
+    $controllerInstance = new $controller();
 
-    case 'detail-lowcomp':
-        require_once __DIR__ . '/public/views/detailpage/lowongancomp/lowongancomp.php';
-        break;
-
-    case 'detail-lowjs':
-        require_once __DIR__ . '/public/views/detailpage/lowonganjs/lowonganjs.php';
-        break;
-
-    case 'lamar':
-        require_once __DIR__ . '/public/views/lamaran/halaman_lamaran/halaman_lamaran.php';
-        break;
-
-    case 'lamar-process':
-        require_once __DIR__ . '/controllers/UploadController.php';
-        break;
-
-    case 'detail-lamaran':
-        require_once __DIR__ . '/public/views/lamaran/detail_lamaran/detail_lamaran.php';
-        break;
-    case 'review-process':
-        require_once __DIR__ . '/controllers/ReviewController.php';
-        break;
-
-    default:
-        header("Location: index.php?page=home");
-        exit();
+    if (method_exists($controllerInstance, $action)) {
+        call_user_func_array([$controllerInstance, $action], $params);
+    } else {
+        echo "Method $method not found in controller $controller";
+    }
+} else {
+    echo "Controller $controller not found";
 }
+
+// spl_autoload_register(function ($class_name) {
+//     require_once __DIR__ . '/models/' . $class_name . '.php';
+// });
+
+// session_start();
+// require_once __DIR__ . '/controllers/LoginController.php';
+
+// $page = isset($_GET['page'])? $_GET['page'] : 'home';
+
+// switch ($page) {
+//     case 'login':
+//         require_once __DIR__ . '/public/views/login/login.php';
+//         break;
+
+//     case 'login-process':
+//         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['password'])) {
+//             $email = $_POST['email'];
+//             $password = $_POST['password'];
+//             $loginController = new LoginController();
+//             $loginController->login($email, $password);
+//         } else {
+//             header("Location: index .php?page=login&error=Invalid request");
+//             exit();
+//         }
+//         break;
+
+//     case 'register':
+//         # code...
+//         break;
