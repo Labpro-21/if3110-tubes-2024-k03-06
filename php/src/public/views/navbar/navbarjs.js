f45ce39f7f53ea2c0f3805bf08e2613c26836971
Rinnearu <rayhanridhar@gmail.com>
2024-10-23 11:26:24 +0700

    refactor: unified the navbars and let unsigned user access home

--- php/src/public/views/navbar/navbarjs.js
+++ php/src/public/views/navbar/navbarjs.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const profileNav = document.getElementById("profile-nav");
-    const dropdownMenu = profileNav.querySelector(".dropdown-menu");
+    if (profileNav) {
+        const dropdownMenu = profileNav.querySelector(".dropdown-menu");
 
-    // Event listener untuk mengklik gambar
-    profileNav.addEventListener("click", function(event) {
-        // Toggle dropdown visibility
-        dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
-        event.stopPropagation(); // Stop click propagation
-    });
+        // Event listener untuk mengklik gambar
+        profileNav.addEventListener("click", function(event) {
+            // Toggle dropdown visibility
+            dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+            event.stopPropagation(); // Stop click propagation
+        });
+    }
 
     // Hide dropdown if clicked outside
     document.addEventListener("click", function() {
