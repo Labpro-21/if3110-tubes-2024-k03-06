5b760a791793db5dc8e2d8d20b460d7c671d435e
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-10-24 12:57:48 +0700

    refactor : detail lamaran display

--- php/src/public/views/lamaran/detail_lamaran/detail_lamaran.php
+++ php/src/public/views/lamaran/detail_lamaran/detail_lamaran.php
@@ -23,65 +23,60 @@ $lamaran = $stmt->fetch();
 </head>
 
 <body>
-    <!-- Desktop Navbar -->
-    <?php
-    echo '<div id="desktop-navbar">';
-    include_once __DIR__ . '/../../navbar/navbarjs.php';
-    echo '</div>';
-    ?>
+        <!-- Desktop Navbar -->
+        <?php
+        echo '<div id="desktop-navbar">';
+        include_once __DIR__ . '/../../navbar/navbarjs.php';
+        echo '</div>';
+        ?>
 
-    <!-- Mobile Navbar -->
-    <nav id="mobile-navbar">
-        <img src="public/assets/img/back.png" alt="Back" class="back-button" onclick="history.back()">
-    </nav>
+        <!-- Mobile Navbar -->
+        <nav id="mobile-navbar">
+            <img src="public/assets/img/back.png" alt="Back" class="back-button" onclick="history.back()">
+        </nav>
 
-    <div class="container">
-        <div class="detail-lamar">
-            <form id="form-review">
-                <h3 id="details">
-                    <?php echo $lamaran['nama'] ?>'s Application Details
-                </h3>
+        <div class="container">
+    <div class="detail-lamar">
+        <form id="form-review">
+        <h3 id="details">
+            <?php echo $lamaran['nama'] ?>'s Application Details
+        </h3>
 
-                <!-- CV -->
-                <h3>
-                    Curriculum Vitae:
-                </h3>
-                <embed class="cv" src="<?php echo $lamaran['cv_path'] ?>" width="auto" height="auto"></embed>
-                <br>
+        <div class="cv-video-container">
+            <!-- CV Box -->
+            <div class="cv-box">
+            <h3>Curriculum Vitae:</h3>
+            <embed class="cv" src="<?php echo $lamaran['cv_path'] ?>"></embed>
+            </div>
 
-                <!-- Video Introduction -->
-                <h3>
-                    Video Introduction:
-                </h3>
-                <video width="100%" height="100%" controls>
-                    <source src="<?php echo $lamaran['video_path'] ?>" type="video/mp4">
-                </video>
-                <br>
-                <br>
-                <!-- Status -->
-                <h3>
-                    Status : <?php echo $lamaran['status'] ?>
-                </h3>
-                <br>
-                <?php
-                if ($lamaran['status'] === 'waiting') {
-                    echo '
-                <label for="message">Message:</label>
-                <div id="editor" style="height: 200px;"></div>
-                <br>
-                <br>
-
-                <label for="status">Final Review:</label>
-                <select id="status" name="status">
-                    <option value="accepted">Accepted</option>
-                    <option value="rejected">Rejected</option>
-                </select>
-                <br>';
-                    echo '<button type="submit" class="submit-application">Finish Review Application</button>';
-                }
-                ?>
-            </form>
+            <!-- Video Box -->
+            <div class="video-box">
+            <h3>Video Introduction:</h3>
+            <video controls>
+                <source src="<?php echo $lamaran['video_path'] ?>" type="video/mp4">
+            </video>
+            </div>
         </div>
+
+        <!-- Status -->
+        <div class="status">
+            <h3>Status: <?php echo $lamaran['status'] ?></h3>
+        </div>
+
+        <?php if ($lamaran['status'] === 'waiting') : ?>
+            <label for="message">Message:</label>
+            <div id="editor" style="height: 200px;"></div>
+
+            <label class="final-review" for="status">Final Review:</label>
+            <select id="status" name="status">
+            <option value="accepted">Accepted</option>
+            <option value="rejected">Rejected</option>
+            </select>
+
+            <button type="submit" class="submit-application">Finish Review Application</button>
+        <?php endif; ?>
+        </form>
+    </div>
     </div>
 
     <script src="public/views/lamaran/detail_lamaran/detail_lamaran.js">
